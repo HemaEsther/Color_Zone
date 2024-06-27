2aeaff43734bf95dd7f6ebc21591048a40bbe36c
HEMA ESTHER SHAH <166860660+HemaEsther@users.noreply.github.com>
2024-06-27 20:50:29 +0530

    Add files via upload

---
+++ color changer/script.js
@@ -0,0 +1,29 @@
+const buttons = document.querySelectorAll('.btn')
+
+const body = document.querySelector('body')
+
+buttons.forEach(function(buttons){
+    buttons.addEventListener('click',function(e){
+        if(e.target.id === 'red'){
+            body.style.backgroundColor = e.target.id;
+        } 
+        else if(e.target.id === 'blue'){
+            body.style.backgroundColor = e.target.id;
+        } 
+        else if(e.target.id === 'green'){
+            body.style.backgroundColor = e.target.id;
+        }
+        else if(e.target.id === 'yellow'){
+            body.style.backgroundColor = e.target.id;
+        }
+        else if(e.target.id === 'orange'){
+            body.style.backgroundColor = e.target.id;
+        }
+        else if(e.target.id === 'violet'){
+            body.style.backgroundColor = e.target.id;
+        }
+        else if(e.target.id === 'pink'){
+            body.style.backgroundColor = e.target.id;
+        }    
+    })
+})
